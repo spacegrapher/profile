aa5088c6a3b2c08efa9a98779f1d3433947eb9c9
Dongyeon Kim <spacegrapher@gmail.com>
2015-06-07 16:58:00 +0900

    Upload initial source code
    Upload source code for my profile page

---
+++ index.php
@@ -0,0 +1,134 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"> 
+<head> 
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"> 
+    <title>Dongyeon Kim</title> 
+    
+    <meta name="description" content="Dongyeon Kim" /> 
+    <meta name="keywords" content="Dongyeon Kim, spacegrapher" /> 
+    <meta name="author" content="Dongyeon Kim" /> 
+    <meta http-equiv="X-UA-Compatible" content="IE=9" /> 
+    
+    <link rel="stylesheet" href="css/base.css" type="text/css" media="screen" charset="utf-8"/> 
+</head> 
+<body id="homepage"> 
+    <div id="header"> 
+        <div id="header-space"> 
+    <div id="logo"> 
+        <span> 
+            <a href="http://www.dongyeonkim.kr" style="text-decoration:none;font-size:28px;color:white"> 
+                Dongyeon Kim
+            </a> 
+        </span> 
+    </div> 
+    <div id="nav"> 
+        <ul> 
+    <li><a href="http://www.dongyeonkim.kr"  class="selected" >Home</a></li> 
+  
+	<li><a href="#education">Education</a></li> 
+    
+	<li><a href="#work">Work Experience</a></li> 
+    
+    <li><a href="#publications">Publications</a></li> 
+    
+   	<li><a href="#info">Additional Info</a></li> 
+</ul>        
+       <div class="rightLower"> 
+            <p> 
+            	<a href="http://www.twitter.com/spacegrapher">twitter</a> &nbsp; &nbsp; 
+                <a href="http://www.facebook.com/spacegrapher">facebook</a> &nbsp; &nbsp;
+                <a href="mailto:spacegrapher@gmail.com">email</a>
+            </p> 
+        </div> 
+    </div> 
+</div> 
+<!----> 
+    </div><!-- end header --> 
+    <div id="body-top"> 
+    <div id="content-top" class="body-width">
+      <div style="display:block;position:relative"> 
+        
+      <div style="width:170px; height:1150px; top:50px; position:relative; left:620px"> 
+
+        <img src="image/profile.jpg" width="170" border="0" 
+            style="border:0;position:absolute;top:0px;z-index:3;left:0px"/></div> 
+      <div style="width:170px; height:170px; position:absolute; left:620px; top:250px; text-align: center;"><script src="http://platform.linkedin.com/in.js" type="text/javascript"></script>
+<script type="IN/MemberProfile" data-id="http://www.linkedin.com/pub/dongyeon-kim/2b/48/33a" data-format="hover" data-text="Dongyeon Kim"></script>
+<p><a href="http://spiff.linknow.kr" target="_blank"><img src="http://www.linknow.kr/weblink/img/profile/banner_120X25.png" width="120" height="25" border="0" alt=""></a></p>
+      </div>
+        <div style="width:610px; height:1150px; position:absolute; top:0; left:0; z-index:4"><!--/div-->
+
+          <h2> 
+          <a name="education" id="education"></a>Education </h2> 
+        <ul>
+          <li> 
+            <strong><a href="http://www.postech.ac.kr" target="_blank">Pohang University of Science and Technology</a></strong>, Pohang, South Korea<br />
+            M.S., Computer Science and Engineering<br />
+            Mar 2007 - Feb 2009<br />
+            <a href="http://cg.postech.ac.kr" target="_blank">Computer Graphics Lab.</a></li>
+          <li><strong><a href="http://www.uwaterloo.ca" target="_blank">University of Waterloo</a></strong>, ON, Canada<br />
+            B.S., Computer Science<br />
+            Aug 2005 - Dec 2005<br />
+            Attended as an Exchange Student
+          </li>
+          <li><strong><a href="http://www.postech.ac.kr" target="_blank">Pohang University of Science and Technology</a></strong>, Pohang, South Korea<br />
+            B.S., Computer Science and Engineering<br />
+            Mar 2003 - Feb 2007
+          </li>
+        </ul>
+        <h2> 
+          <a name="work" id="work"></a>Work  Experience </h2> 
+        <ul>
+        <li> 
+            <a href="http://www.samsung.com/sec" target="_blank"><strong>Samsung Electronics</strong></a>, Suwon, South Korea<br />
+            Engineer<br />
+            Oct 2011
+            - Present<br />
+            Tizen Platform, Graphics</li>
+          <li> 
+            <a href="http://www.digitalaria.com" target="_blank"><strong>Digital Aria</strong></a>, Seongnam, South Korea<br />
+            Assistant Research Engineer<br />
+            Jan 2009
+            - Sep 2011<br />
+            Developing OpenGL ES, OpenVG-based 3D rendering engine for the embedded GUI middleware solution that runs on various platforms such as Android and Linux.</li>
+        </ul>
+<h2> 
+          <a name="publications" id="publications"></a>Publications </h2>
+<ul>
+  <li> Dongyeon Kim, Minjung Son, Yunjin Lee, Henry Kang, and Seungyong Lee, <em><strong><a href="http://cg.postech.ac.kr/research/stippling/" target="_blank">&quot;Feature-guided Image Stippling&quot;</a></strong></em>, Computer Graphics Forum (Special issue on the Eurographics Symposium on Rendering), vol. 27, no. 4, pp. 1209-1216, June 2008. </li>
+  <li> Kyuman Jeong, Dongyeon Kim, Soon-Yong Park, and Seungyong Lee, <em><strong><a href="http://www.springerlink.com/content/lj53w450u32h8g01/fulltext.pdf" target="_blank">&quot;Digital shallow depth-of-field adapter for photographs,&quot;</a></strong></em> The Visual Computer (Special issue on the Proc. of the 7th Korea-Israel Bi-National Conference on Geometric Modeling and Computer Graphics), vol. 24, no. 4, April 2008. </li>
+</ul>
+<h2><a name="info" id="info"></a>Additional Information </h2>
+        <ul>
+          <li>Creator of iOS Application &quot;Airport Info Korea&quot;<br>
+            <a href="http://itunes.apple.com/gb/app/id379784630?mt=8" target="_blank">http://itunes.apple.com/gb/app/id379784630?mt=8</a></li>
+          <li>Active TED Translator<br>
+            <a href="http://www.ted.com/profiles/444483" target="_blank">http://www.ted.com/profiles/444483</a><br>
+          </li>
+        </ul>
+        </div> 
+      </div> 
+    </div><!-- end content-top --> 
+</div><!-- end body-top --> 
+    
+    <div id="footer"> 
+        <div class="container"> 
+   <div id="almostEnd"> 
+        <a href="http://www.dongyeonkim.kr">Home</a> &nbsp;|&nbsp; 
+        <a href="#education">Education</a> &nbsp;|&nbsp; 
+        <a href="#work">Work Experience</a> &nbsp;|&nbsp; 
+        <a href="#publications">Publications</a> &nbsp;|&nbsp; 
+        <a href="#info">Additional Info</a> 
+    </div> 
+    <div id="end"> 
+        <a href="http://twitter.com/spacegrapher">twitter</a> &nbsp;
+        <a href="http://www.facebook.com/spacegrapher">facebook</a> &nbsp;
+        <a href="mailto:spacegrapher@gmail.com">email</a> 
+    </div> 
+    <div id="legal"> 
+        <p> 
+            Dongyeon Kim</p> 
+    </div> 
+</div> 
+    </div><!-- end footer --> 
+</body> 
+</html> 
